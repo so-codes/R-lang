f76908cca5acf69179976140db52ac1701eddafa
crizmo <crizmo595@gmail.com>
2022-07-16 19:57:25 +0530

    Initial commit

---
+++ assign/assign_2.R
@@ -0,0 +1,67 @@
+# practical 1
+v1 <- c(1,3,5,7,9,11)
+m1 <- matrix(v1 ,nrow = 3, ncol = 2, byrow = TRUE)
+m1
+
+# practical 2
+m2 <- matrix(c(12:20),nrow = 3, ncol = 3, byrow = FALSE)
+m2
+
+# practical 3
+
+# pr 3 -1
+v2 <- c(1,3,5,7,9,11,13,15,17)
+m1 <- matrix(v2 ,nrow = 3, ncol = 3, byrow = TRUE) 
+m1 + m2
+m1 - m2
+
+# pr 3 - 2
+m1 * m2
+
+# pr 3 - 3
+m1 %*% m2
+
+# pr 3 - 4
+t(m1)
+
+# pr 3 - 5
+diag(m2)
+
+# pr 4
+m2[3,]
+
+#pr 5
+v3 <- m1 - 2*m2
+print(v3[,2])
+
+# pr 6
+t(m2+m1)
+
+# pr 7
+v4 <- 4*m1*m2
+print(v4[3,2])
+
+# pr 8
+row_names = c("row1", "row2", "row3")
+col_names = c("col1", "col2", "col3")
+
+m1 <- matrix(v2 ,nrow = 3, ncol = 3, byrow = TRUE, dimnames = list(row_names, col_names))
+m1
+
+# pr 9
+solve(m1)
+solve(m2)
+
+# pr 10
+v1 <- c(1,3,5,7,9,11)
+m1 <- matrix(v1 ,nrow = 2, ncol = 2, byrow = TRUE)
+v5 <- 2*m1
+solve(v5)
+
+# pr 11
+v6 <- 3*m1 + 2*m2
+v7 <- 10*m2 - m1
+v8 <- m1/4
+v6
+v7
+v8
---
+++ assign/r1.r
@@ -0,0 +1,10 @@
+# sum1 
+a <- 10
+b <- 30
+c <- TRUE
+
+c <- a * b / 10 
+
+a|b&c
+a|b|(!c)
+a+b&c
