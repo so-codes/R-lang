ca5937a099d08fc7ed9f077f51c6ad8af05c3e37
crizmo <crizmo595@gmail.com>
2022-07-30 19:39:49 +0530

    Update pr4.R

--- pract/pr4.R
+++ pract/pr4.R
@@ -3,11 +3,9 @@ w1 <- c(1,2,3,4,5,6,7,8,9,10)
 w2 <- c(1,2,3,4,5,6,NA,NA,NA,NA)
 w3 <- c(1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8)
 
-mean(w2,na=TRUE)
-
-
 # mct
 mean(w1)
+mean(w2,na=TRUE)
 tm <- mean(w3, trim = 0.2)
 print(tm)
 
@@ -27,26 +25,52 @@ Mode(w3)
 # pr 2
 er <- faithful$eruptions
 
+# 1
 mean(er)
 median(er)
 Mode(er)
 
+# 2
 boxplot(er)
 
+# 3
 quantile(er,c(.37,.56,.78))
 
 
 # pr 4
 wt <- mtcars
 
+# 1
 mean(mtcars$wt)
 median(mtcars$wt)
 Mode(mtcars$wt)
-quantile(mtcars$wt,probs=seq(.1,.9,by=.1))
 
+# 2
+quantile(mtcars$wt,probs=seq(.1,.9,by=.1))
 
 boxplot(mtcars$mpg)
 
+# 4
 quantile(mtcars$wt)
 
+# 5
 summary(mtcars$wt)
+
+# pr 6
+name <- c("Kurizu", "Bob", "Jane", "Pocky")
+age <- c(18,20,19,17)
+weight <- c(55,54,57,56)
+
+# 1
+summary(name)
+summary(age)
+summary(weight)
+
+# 2
+median(age)
+
+# 3
+quantile(weight,c(.92))
+
+# 4
+boxplot(age)
